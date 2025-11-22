fb36b8ea5352d2adc557cf7b01f201bc70eecd0e
Alicetear <phrnakanth@gmail.com>
2025-11-22 22:28:20 +0700

    Edit

--- Assets/Scripts/MapTransation.cs
+++ Assets/Scripts/MapTransation.cs
@@ -1,11 +1,17 @@
 using Cinemachine;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MapTransation : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D mapBoundry;
-    [SerializeField] CinemachineConfiner2D confiner;  
+    [SerializeField] CinemachineConfiner2D confiner;
+    [SerializeField] string nextSceneName;
+    [SerializeField] Direction direction;  
+    [SerializeField] float additivePos = 2f;
+
+    enum Direction { up, down, Left, Right }
 
     [System.Obsolete]
     private void Awake()
@@ -18,10 +24,45 @@ public class MapTransation : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player") && confiner != null)
+        if (collision.CompareTag("Player"))
         {
             confiner.m_BoundingShape2D = mapBoundry;
             confiner.InvalidateCache();
+
+            UpdatePlayerPosition(collision.gameObject);
+            if (!string.IsNullOrEmpty(nextSceneName))
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
         }
     }
+
+
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 newPos = player.transform.position;
+
+        switch (direction) 
+        {
+            case Direction.up:
+                newPos.y += additivePos;
+                break;
+            case Direction.down:
+                newPos.y -= additivePos;
+                break;
+            case Direction.Left:
+                newPos.x += additivePos;
+                break;
+            case Direction.Right:
+                newPos.x -= additivePos;
+                break;
+        }
+
+        player.transform.position = newPos;
+    }
+
+
+
+
+
 }
